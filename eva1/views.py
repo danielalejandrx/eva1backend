419b71e873fe4b018d6db75db1cb193d4d76eb5f
Andy <danielalejandra.css@gmail.com>
2023-05-07 23:25:56 -0400

    3push

--- eva1/views.py
+++ eva1/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def index(request):
 
-    return render(request,'template/index.html')
+    return render(request, 'template/index.html')
 
 def juegos(request):
 
